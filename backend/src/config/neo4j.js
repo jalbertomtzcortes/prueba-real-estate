ae95176bd2c4245f8315fa048f13a84433875d40
Jose Alberto Martinez Cortes <jalbertomtzcortes@hotmail.com>
2026-03-05 23:01:21 -0600

    flujo correcto neo

--- backend/src/config/neo4j.js
+++ backend/src/config/neo4j.js
@@ -1,1 +1,9 @@
+const neo4j = require("neo4j-driver");
 
+const uri = process.env.NEO4J_URI || "bolt://neo4j:7687";
+const user = process.env.NEO4J_USER || "neo4j";
+const password = process.env.NEO4J_PASSWORD || "admin123";
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+module.exports = driver;
